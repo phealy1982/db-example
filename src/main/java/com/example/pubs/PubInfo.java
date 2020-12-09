e7f8b1142962184eb85b884dcbf8d3ff052d47ba
phealy1982 <phealy1982@gmail.com>
2020-12-09 19:34:53 +0000

    second commit

---
+++ src/main/java/com/example/pubs/PubInfo.java
@@ -0,0 +1,58 @@
+package com.example.pubs;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class PubInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String address;
+    private boolean beerGarden = false;
+    private String openingHours;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean isBeerGarden() {
+        return beerGarden;
+    }
+
+    public void setBeerGarden(boolean beerGarden) {
+        this.beerGarden = beerGarden;
+    }
+
+    public String getOpeningHours() {
+        return openingHours;
+    }
+
+    public void setOpeningHours(String openingHours) {
+        this.openingHours = openingHours;
+    }
+}
---
+++ src/main/java/com/example/pubs/PubsApplication.java
@@ -0,0 +1,13 @@
+package com.example.pubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubsApplication.class, args);
+	}
+
+}
